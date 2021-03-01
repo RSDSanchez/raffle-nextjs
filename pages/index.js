fae15bc170b33c1fd1bd7feedfd98ed96e8c22b5
geuv <geuvadan@gmail.com>
2021-03-01 11:14:05 +0100

    again

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,9 @@ import { useState, useEffect } from 'react';
 import Description from '../components/Description/Description';
 
 export async function getStaticProps() {
-  const data = await fetch(process.env.DATA_JSON_LOCATION, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const data = await fetch(process.env.DATA_JSON_LOCATION);
   const dataToJSON = await data.json();
   const raffles = dataToJSON.products;
-  raffles = JSON.parse(raffles);
 
   return {
     props: {
