cc5998d378fef8849cded9161f9bef8031c9f351
geuv <geuvadan@gmail.com>
2021-04-30 08:39:15 +0200

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -24,15 +24,14 @@ function MyApp({ Component, pageProps }) {
         />
 
         {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-60363125-1"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'UA-60363125-1');
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-59DTL2X');
                   `,
           }}
         />
@@ -40,7 +39,25 @@ function MyApp({ Component, pageProps }) {
 
       <RecoilRoot>
         <GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-59DTL2X"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <Layout>
+            {/* <!-- Google Tag Manager (noscript) --> */}
+            <noscript>
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-59DTL2X"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              ></iframe>
+            </noscript>
+            {/* <!-- End Google Tag Manager (noscript) --> */}
             <Component {...pageProps} />
           </Layout>
         </GoogleReCaptchaProvider>
